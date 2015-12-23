bf9588f568a112366de1ed4674d8b13044ffc45e
Sulverus <sulverus@gmail.com>
2015-12-23 21:54:19 +0300

    gh-2 tested docker images and memcached application

--- docker/app.lua
+++ docker/app.lua
@@ -1,11 +1,16 @@
 #!/usr/bin/env tarantool
 local EXPOSED_PORT = 3301
+local ADMIN_PORT = 3302
 local memcached = require('memcached')
 
 box.cfg{
     slab_alloc_arena = 0.5;
     wal_mode = 'none';
+    listen = ADMIN_PORT;
 }
 
+-- enable admin provison
+box.schema.user.grant('guest', 'read,write,execute', 'universe')
+
 -- start memcached instance
-instance = memcached.create('local_instance', '0.0.0.0:' .. tostring(EXPOSED_PORT))
+instance = memcached.create('instance', '0.0.0.0:' .. tostring(EXPOSED_PORT))
