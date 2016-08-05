278afca0ee69b6e2a646034a4bf50282f15ebffe
Konstantin Nazarov <mail@racktear.com>
2016-08-05 11:24:53 +0300

    Moniroting should connect to port 3301

--- docker/tarantool-cloud-memcached/tarantool_replication.sh
+++ docker/tarantool-cloud-memcached/tarantool_replication.sh
@@ -3,7 +3,7 @@
 replication_status=$(tarantool <<-'EOF'
 os = require("os")
 netbox = require('net.box')
-conn = netbox.new("localhost:3302")
+conn = netbox.new("localhost:3301")
 print(conn:eval('return box.info.replication.status'))
 os.exit()
 EOF
