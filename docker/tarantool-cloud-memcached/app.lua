e14ea46bb7aa314c3496cfdd01e800244661c07a
Konstantin Nazarov <mail@racktear.com>
2016-08-22 19:31:05 +0300

    Report memcached stats to prometheus

--- docker/tarantool-cloud-memcached/app.lua
+++ docker/tarantool-cloud-memcached/app.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env tarantool
 
-http = require('http.server')
-prometheus = require('tarantool-prometheus')
+local http = require('http.server')
+local prometheus = require('tarantool-prometheus')
 
 local EXPOSED_PORT = 11211
 local memcached = require('memcached')
@@ -23,10 +23,51 @@ box.once(
     end
 )
 
--- start memcached instance
-instance = memcached.create('instance', '0.0.0.0:' .. tostring(EXPOSED_PORT))
+function collect_stats()
+    local instance = memcached.get("instance")
 
-httpd = http.new('0.0.0.0', 8080)
+    local requests_total = prometheus.gauge(
+        'memcached_requests_total',
+        'Total number of memcached requests by request type',
+        {'request_type'})
+
+    local read_bytes = prometheus.gauge(
+        'memcached_read_bytes',
+        'Total number of bytes read')
+
+    local write_bytes = prometheus.gauge(
+        'memcached_write_bytes',
+        'Total number of bytes written')
+
+    local request_tags = {
+        'cmd_get', 'get_hits', 'get_misses',
+        'cmd_delete', 'delete_hits', 'delete_misses',
+        'cmd_set', 'cas_hits', 'cas_badval', 'cas_misses',
+        'cmd_incr', 'incr_hits', 'incr_misses',
+        'cmd_decr', 'decr_hits', 'decr_misses',
+        'cmd_touch', 'touch_hits', 'touch_misses',
+        'cmd_flush'}
+
+    while true do
+        local stats = instance:info()
+
+        for _, v in pairs(request_tags) do
+            requests_total:set(tonumber(stats[v]), {v})
+        end
+
+        read_bytes:set(tonumber(stats['bytes_read']))
+        write_bytes:set(tonumber(stats['bytes_written']))
+
+        fiber.sleep(5)
+    end
+end
+
+-- start memcached instance
+memcached.create('instance', '0.0.0.0:' .. tostring(EXPOSED_PORT))
+
+fiber.create(collect_stats)
+
+local httpd = http.new('0.0.0.0', 8080)
 
 prometheus.init()
 
