d8fbdcf16cfe6f9e8c887f1efec0743969da6687
Konstantin Nazarov <mail@racktear.com>
2016-12-02 15:06:42 +0300

    Add prometheus metrics to Tarantool

--- docker/tarantool-cloud-tarantool/app.lua
+++ docker/tarantool-cloud-tarantool/app.lua
@@ -3,9 +3,19 @@
 local fio = require('fio')
 local log = require('log')
 local os = require('os')
+local http = require('http.server')
+local prometheus = require('tarantool-prometheus')
 
 local APP_FILE_PATH = '/opt/tarantool/app.lua'
 local app_file_exists = fio.stat(APP_FILE_PATH) ~= nil
+
+local httpd = http.new('0.0.0.0', 8080)
+
+prometheus.init()
+
+httpd:route( { path = '/metrics' }, prometheus.collect_http)
+httpd:start()
+
 
 if not app_file_exists then
     log.info("No app present ('%s'). Running in 'database mode'", APP_FILE_PATH)
